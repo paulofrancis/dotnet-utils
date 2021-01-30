10713a0c3e6aac1f73e9467bc4b5d3f2574800b6
Paulo Francis <Paulo.DaSilva@blueinsurance.ie>
2021-01-30 14:36:49 +0000

    Add project files.

---
+++ dotnet-utils/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_utils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
