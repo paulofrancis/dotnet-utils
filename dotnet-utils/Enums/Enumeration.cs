56102cc7ac0563963373ccf94b82b3bce3d515b7
Paulo Francis <Paulo.DaSilva@blueinsurance.ie>
2021-02-15 10:29:56 +0000

    Enumeration added

---
+++ dotnet-utils/Enums/Enumeration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace dotnet_utils.Enums
+{
+    public abstract class Enumeration : IComparable
+    {
+        public string Name { get; private set; }
+        public int Id { get; private set; }
+
+        protected Enumeration(int id, string name) => (Id, Name) = (id, name);
+
+        public override string ToString() => Name;
+
+        public static IEnumerable<T> GetAll<T>() where T : Enumeration => typeof(T)
+            .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly)
+            .Select(f => f.GetValue(null))
+            .Cast<T>();
+
+        public int CompareTo(object other) => Id.CompareTo(((Enumeration)other).Id);
+
+        public override bool Equals(object obj)
+        {
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = (typeMatches && Id.Equals(((Enumeration)obj).Id));
+
+            return typeMatches && valueMatches;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ dotnet-utils/Enums/Example.cs
@@ -0,0 +1,11 @@
+﻿namespace dotnet_utils.Enums
+{
+    public class Example : Enumeration
+    {
+        public static Example Example1 = new Example(1, nameof(Example1));
+        public static Example Example2 = new Example(2, nameof(Example2));
+        public static Example Example3 = new Example(3, nameof(Example3));
+
+        public Example(int id, string name) : base(id, name) { }
+    }
+}
